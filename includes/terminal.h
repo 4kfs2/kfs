6b6de07a2cd3256771ba79ca11ee30b8f689f5c4
hoseong511 <thdghtjd115@naver.com>
2023-11-06 15:51:57 +0900

    refact : key.c -> keyboard.c keyboard.h

--- includes/terminal.h
+++ includes/terminal.h
@@ -2,6 +2,7 @@
 #define __TERMINAL_H__
 
 #include "vga.h"
+#include "keyboard.h"
 #include <stddef.h>
 
 void terminal_initialize(void);
@@ -10,6 +11,5 @@ void terminal_putentryat(char, uint8_t, size_t, size_t);
 void terminal_putchar(char);
 void terminal_write(const char*, size_t);
 void terminal_writestring(const char*);
-void update_cursor(int x, int y);
 
 #endif
