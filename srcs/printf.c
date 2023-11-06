2974c3d6d034a2a53f0cf28f6bd360c000969e17
Jiwoo Ahn <ilp_rincipe@naver.com>
2023-11-06 15:06:38 +0900

    Fix: va_list param type & num print

--- srcs/printf.c
+++ srcs/printf.c
@@ -5,10 +5,10 @@
 
 static const char chars[16] = "0123456789ABCDEF";
 
-static int print_str(va_list args)
+static int print_str(va_list *args)
 {
 	int r = 0;
-	char *s = va_arg(args, char*);
+	char *s = va_arg(*args, char*);
 	if (s == NULL)
 		s = "(NULL)";
 	terminal_writestring(s);
@@ -16,40 +16,67 @@ static int print_str(va_list args)
 	return r;
 }
 
-static int print_hex(va_list args)
+static unsigned int get_quot(unsigned int num, int base)
+{
+	unsigned quotient = 1;
+
+	while (num /= base)
+		quotient *= base;
+	return quotient;
+}
+
+static int print_num(long num, int base)
 {
 	int r = 0;
-	unsigned int x = va_arg(args, unsigned int);
+	if (num <= 0)
+	{
+		if (num == 0)
+		{
+			terminal_putchar('0');
+			return 1;
+		}
+		else
+		{
+			terminal_putchar('-');
+			++r;
+			num *= -1;
+		}
+	}
+
+	unsigned int quotient = get_quot(num, base);
+	while (num)
+	{
+		terminal_putchar(chars[num / quotient]);
+		num %= quotient;
+		quotient /= base;
+		++r;
+	}
+	return r;
+}
+
+static int print_hex(va_list *args)
+{
+	int r = 0;
+	unsigned int x = va_arg(*args, unsigned int);
 
 	terminal_writestring("0x");
 	r += 2;
-	while (x)
-	{
-		terminal_putchar(chars[x % 16]);
-		x /= 16;
-		++r;
-	}
+	r += print_num(x, 16);
 	return r;
 }
 
-static int print_int(va_list args)
+static int print_int(va_list *args)
 {
 	int r = 0;
-	int d = va_arg(args, int);
-
-	while (d)
-	{
-		terminal_putchar(chars[d % 10]);
-		d /= 10;
-		++r;
-	}
+	int d = va_arg(*args, int);
+	r += print_num(d, 10);
 	return r;
 }
 
-static int print_char(va_list args)
+static int print_char(va_list *args)
 {
 	int r = 0;
-	int c = va_arg(args, int);
+	int c = va_arg(*args, int);
 	terminal_putchar(c);
 	++r;
 	return r;
@@ -76,22 +103,25 @@ static int vprintf(const char *fmt, va_list args)
 			switch (*fmt)
 			{
 				case 's':
-					r += print_str(args); break;
+					r += print_str(&args); break;
 				case 'd':
-					r += print_int(args); break;
+					r += print_int(&args); break;
 				case 'x':
-					r += print_hex(args); break;
+					r += print_hex(&args); break;
 				case 'c':
-					r += print_char(args); break;
+					r += print_char(&args); break;
 				case '%':
 					r += print_percent(); break;
 				default:
 					goto print_c;
 			}
 		}
-		print_c:
-			terminal_putchar(*fmt);
-			++r;
+		else
+		{
+			print_c:
+				terminal_putchar(*fmt);
+				++r;
+		}
 		++fmt;
 	}
 	return r;
