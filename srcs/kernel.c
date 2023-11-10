14a4b6414396ea81d30ec0aada111900858c390b
hoseong511 <thdghtjd115@naver.com>
2023-11-10 17:05:45 +0900

    test : descriptor src from osdev

--- srcs/kernel.c
+++ srcs/kernel.c
@@ -1,9 +1,15 @@
 #include "../includes/utils.h"
 #include "../includes/terminal.h"
+#include "desc.h"
 
 void kernel_main(void) 
 {
 	terminal_initialize();
 	terminal_writestring("42\n");
-	keyboard();
+	create_descriptor(0, 0, 0);
+    create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL0));
+    create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL0));
+    create_descriptor(0, 0x000FFFFF, (GDT_CODE_PL3));
+    create_descriptor(0, 0x000FFFFF, (GDT_DATA_PL3));
+	// keyboard();
 }
