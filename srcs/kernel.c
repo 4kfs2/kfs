32db3562ec8b2fe26908fb4c0e1e04e7177e1d45
ilp-sys <ilp_rincipe@naver.com>
2023-11-18 07:48:34 +0900

    20 parse elf sym table (#22)
    * Feat: multiboot2 header from gnu
    
    * Feat: elf tag parser
    
    * Feat: support kernel_main argument
    
    * Add: elf.c to srcs
    
    * Feat: memcpy

--- srcs/kernel.c
+++ srcs/kernel.c
@@ -1,9 +1,12 @@
 #include "../includes/utils.h"
 #include "../includes/terminal.h"
+#include "../includes/elf.h"
 
-void kernel_main(void) 
+void kernel_main(unsigned long addr)
 {
 	terminal_initialize();
 	terminal_writestring("42\n");
+
+	parse_elf(addr);
 	keyboard();
 }
