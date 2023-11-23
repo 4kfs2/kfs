66b7da7e5614e7622056d0c73783b1caa0f25afd
hoseong511 <thdghtjd115@naver.com>
2023-11-23 15:13:06 +0900

    Refact: define port

--- includes/terminal.h
+++ includes/terminal.h
@@ -5,6 +5,12 @@
 #include "keyboard.h"
 #include <stddef.h>
 
+# define CURSOR_INDEX_PORT		0x3D4
+# define CURSOR_DATA_PORT		0x3D5
+
+# define UPPERCURSOR 0x0F
+# define LOWERCURSOR 0x0E
+
 void terminal_initialize(void);
 void terminal_setcolor(uint8_t);
 void terminal_putentryat(unsigned char, uint8_t, size_t, size_t);
