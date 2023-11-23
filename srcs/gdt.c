d5575ca1a4a8dffc2e54ceaa393aa1e002bbdbf1
hoseong511 <thdghtjd115@naver.com>
2023-11-23 12:53:31 +0900

    Feat: change access bit

--- srcs/gdt.c
+++ srcs/gdt.c
@@ -9,9 +9,9 @@ void init_gdt_desc(uint32_t base, uint32_t limit, uint8_t acces, uint8_t other, 
 	desc->lim0_15 = (limit & 0xffff);
 	desc->base0_15 = (base & 0xffff);
 	desc->base16_23 = (base & 0xff0000) >> 16;
-	desc->acces = acces;
+	desc->acces = acces; // P(present 1? valid seg) DPL(privilege level 0/3) S(type 1? code/data) E(1? code) DC RW A
 	desc->lim16_19 = (limit & 0xf0000) >> 16;
-	desc->other = (other & 0xf);
+	desc->other = (other & 0xf); // G(Granularity 1? 4KB) DB(size 1? 32bit) L(1? 64bit) AVL(reserved)
 	desc->base24_31 = (base & 0xff000000) >> 24;
 	return;
 }
@@ -19,13 +19,13 @@ void init_gdt_desc(uint32_t base, uint32_t limit, uint8_t acces, uint8_t other, 
 void init_gdt(void)
 {
 	init_gdt_desc(0x0, 0x0, 0x0, 0x0, &kgdt[0]);
-	init_gdt_desc(0x0, 0xFFFFF, 0x9B, 0x0D, &kgdt[1]); //kernel_code
-	init_gdt_desc(0x0, 0xFFFFF, 0x93, 0x0D, &kgdt[2]); // kernel_data
-	init_gdt_desc(0x0, 0x0, 0x97, 0x0D, &kgdt[3]); // kernel_stack
+	init_gdt_desc(0x0, 0xFFFFF, 0x9A, 0x0C, &kgdt[1]); //kernel_code
+	init_gdt_desc(0x0, 0xFFFFF, 0x92, 0x0C, &kgdt[2]); // kernel_data
+	init_gdt_desc(0x0, 0x0, 0x97, 0x0C, &kgdt[3]); // kernel_stack
 
-	init_gdt_desc(0x0, 0xFFFFF, 0xFF, 0x0D, &kgdt[4]); //u_code
-	init_gdt_desc(0x0, 0xFFFFF, 0xF3, 0x0D, &kgdt[5]); //u_data
-	init_gdt_desc(0x0, 0x0, 0xF7, 0x0D, &kgdt[6]); //u_stack
+	init_gdt_desc(0x0, 0xFFFFF, 0xFA, 0x0C, &kgdt[4]); //u_code
+	init_gdt_desc(0x0, 0xFFFFF, 0xF2, 0x0C, &kgdt[5]); //u_data
+	init_gdt_desc(0x0, 0x0, 0xF7, 0x0C, &kgdt[6]); //u_stack
 
 	 kgdtr.limit = GDTSIZE * 8;
 	 kgdtr.base = GDTBASE;
