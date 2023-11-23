4eda3f0e8ede3b408e54d9b588aa0fc5a36c7dcb
ilp-sys <ilp_rincipe@naver.com>
2023-11-23 14:53:02 +0900

    25 simple shell (#29)
    * Feat: added out_buf_full (#26)
    
    * Feat: added out_buf_full (#26)
    
    * Feat: reboot (#28)
    
    * Feat: shell
    
    ---------
    
    Co-authored-by: Hoseong Song <62678380+hoseong511@users.noreply.github.com>

--- includes/keyboard.h
+++ includes/keyboard.h
@@ -1,7 +1,18 @@
 #ifndef __KEYBOARD_H__
-#define __KEYBOARD_H__
+# define __KEYBOARD_H__
 
-void keyboard();
-void update_cursor(int x, int y);
+# include <stdint.h>
+
+# define KEYBOARD_COMMAND_PORT	0x64
+# define KEYBOARD_DATA_PORT		0x60
+
+# define KBRD_RESET 0xFE
+
+void	update_cursor(int, int);
+
+int		getchar();
+
+void	outb(uint16_t, uint8_t);
+uint8_t	inb(uint16_t);
 
 #endif
