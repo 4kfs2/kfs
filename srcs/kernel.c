19e84b7c3c2b141906ffa051a26dcf040aa0da77
hoseong511 <thdghtjd115@naver.com>
2023-12-22 19:03:18 +0900

    test: added mmap for test

--- srcs/kernel.c
+++ srcs/kernel.c
@@ -1,14 +1,10 @@
 #include "../includes/utils.h"
 #include "../includes/terminal.h"
 #include "../includes/elf.h"
+#include "multiboot2.h"
 #include "debug.h"
 
 # define CMD_SIZE 256
-
-uint32_t page_directory[1024] __attribute__((aligned(4096)));
-uint32_t first_page_table[1024] __attribute__((aligned(4096)));
-uint32_t second_page_table[1024] __attribute__((aligned(4096)));
-uint32_t third_page_table[1024] __attribute__((aligned(4096)));
 
 static void runcmd(int index, char *cmd)
 {
@@ -31,11 +27,55 @@ void func()
 	func2();
 }
 
+void mmap(unsigned addr)
+{
+	struct multiboot_tag *tag;
+	unsigned size;
+
+	size = *(unsigned *) addr;
+  	printf ("Announced mbi size 0x%x\n", size);
+	for (tag = (struct multiboot_tag *) (addr + 8);
+       tag->type != MULTIBOOT_TAG_TYPE_END;
+       tag = (struct multiboot_tag *) ((multiboot_uint8_t *) tag 
+                                       + ((tag->size + 7) & ~7)))
+    {
+		switch (tag->type)
+		{
+		case MULTIBOOT_TAG_TYPE_MMAP:
+			{
+			multiboot_memory_map_t *mmap;
+
+			printf ("mmap\n");
+		
+			for (mmap = ((struct multiboot_tag_mmap *) tag)->entries;
+				 (multiboot_uint8_t *) mmap 
+					 < (multiboot_uint8_t *) tag + tag->size;
+				 mmap = (multiboot_memory_map_t *) 
+					 ((unsigned long) mmap + ((struct multiboot_tag_mmap *) tag)->entry_size))
+				printf (" base_addr = 0x%x%x,"
+						" length = 0x%x%x, type = 0x%x (%s) \n", 
+						(unsigned) (mmap->addr >> 32),
+						(unsigned) (mmap->addr & 0xffffffff),
+						(unsigned) (mmap->len >> 32),
+						(unsigned) (mmap->len & 0xffffffff),
+						(unsigned) mmap->type,
+						mmap->type == 2 ? "reserved" : "available");
+			}
+			break;
+		}
+	}
+  tag = (struct multiboot_tag *) ((multiboot_uint8_t *) tag 
+                                  + ((tag->size + 7) & ~7));
+
+  printf ("Total mbi size 0x%x\n", (unsigned) tag - addr);
+}
+
 void kernel_main(unsigned long addr)
 {
 	// init_gdt();
 	terminal_initialize();
-	// printf("%x\n", addr);
+	printf("%x\n", addr);
+	mmap(addr + 0x80000000);
 	parse_elf(addr + 0x80000000);
 	func();
 	while (1)
