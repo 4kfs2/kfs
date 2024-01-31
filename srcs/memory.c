2bb041b260e71173e5ae74a3a01cec18c65e428c
hoseong511 <thdghtjd115@naver.com>
2024-01-31 16:30:11 +0900

    feat: init mapping area(1)
    - bitmap : set, unset, test
    - bitmap_alloc

--- srcs/memory.c
+++ srcs/memory.c
@@ -1,15 +1,41 @@
+#include "../includes/mm.h"
+#include "utils.h"
+#include "panic.h"
 
-unsigned long *pg_dir;
-unsigned long *pg_tbl;
+#define IDX_FRAME(a) (a/(4*8))
+#define OFFSET_FRAME(a) (a%(4*8))
 
-void paging_init()
+uint32_t placement = &_kernel_end;
+struct meminfo mmi;
+
+static void set_bit(uint32_t addr)
 {
-	for(unsigned i = 0; i < 1024; i++)
-	{
-		pg_dir[i] = (unsigned long)(pg_tbl + 1024 * i) | 0x3;
-	}
-	for(unsigned i = 0; i < 1024; i++)
-	{
-		pg_tbl[i] = (i * 0x1000) | 0x1;
-	}
+	uint32_t frame = addr / 0x1000;
+	uint32_t idx = IDX_FRAME(frame);
+	uint32_t off = OFFSET_FRAME(frame);
+	mmi.frames[idx] |= (0x1 << off);
 }
+
+static void unset_bit(uint32_t addr)
+{
+	uint32_t frame = addr / 0x1000;
+	uint32_t idx = IDX_FRAME(frame);
+	uint32_t off = OFFSET_FRAME(frame);
+	mmi.frames[idx] &= ~(0x1 << off);
+}
+
+static uint32_t test_bit(uint32_t addr)
+{
+	uint32_t frame = addr / 0x1000;
+	uint32_t idx = IDX_FRAME(frame);
+	uint32_t off = OFFSET_FRAME(frame);
+	return (mmi.frames[idx] & (0x1 << off));
+}
+
+uint32_t bitmap_alloc(uint32_t size)
+{
+	uint32_t start = placement;
+	placement += size;
+	return start;
+}
+
