689951cab9b428b758e7e09eca260123b37e0eb1
hoseong511 <thdghtjd115@naver.com>
2024-02-06 16:56:35 +0900

    feat: added select, insert ordered_array
    - added panic_1 (이전 커밋)

--- includes/ordered_array.h
+++ includes/ordered_array.h
@@ -13,5 +13,7 @@ typedef struct ordered_array
 } ordered_array_t;
 
 ordered_array_t init_ordered_array(void *start, uint32_t max_size, lessthan_predic_t less);
+void insert_ordered_array(type_t item, ordered_array_t *arr);
+type_t select_ordered_array(uint32_t idx, ordered_array_t *arr);
 
 #endif
--- srcs/ordered_array.c
+++ srcs/ordered_array.c
@@ -1,5 +1,6 @@
 #include "ordered_array.h"
 #include "utils.h"
+#include "panic.h"
 
 ordered_array_t init_ordered_array(void *start, uint32_t max_size, lessthan_predic_t less)
 {
@@ -10,4 +11,33 @@ ordered_array_t init_ordered_array(void *start, uint32_t max_size, lessthan_pred
 	new.max_size = max_size;
 	new.lessthan = less;
 	return new;
+}
+
+void insert_ordered_array(type_t item, ordered_array_t *arr)
+{
+	uint32_t iterator = 0;
+	while (iterator < arr->size && arr->lessthan(arr->arr[iterator], item))
+		iterator++;
+	if (iterator == arr->size)
+		arr->arr[arr->size++] = item;
+	else
+	{
+		type_t tmp = arr->arr[iterator];
+		arr->arr[iterator] = item;
+		while (iterator < arr->size)
+		{
+			iterator++;
+			type_t tmp2 = arr->arr[iterator];
+			arr->arr[iterator] = tmp;
+			tmp = tmp2;
+		}
+		arr->size++;
+	}
+}
+
+type_t select_ordered_array(uint32_t idx, ordered_array_t *arr)
+{
+	if (idx >= arr->size) // for debug
+		panic_1("check arr_size, idx");
+	return arr->arr[idx];
 }
