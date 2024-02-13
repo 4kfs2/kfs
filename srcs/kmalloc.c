421e729f0ffbc61c3ef79c5b418396721c03b4ed
hoseong511 <thdghtjd115@naver.com>
2024-02-13 13:16:29 +0900

    feat: alloc(hole->block)
    - hole의 크기 보다 작게 할당될 경우
    - align이 필요시 기존 hole 수정
    - 아니면 hole 삭제

--- srcs/kmalloc.c
+++ srcs/kmalloc.c
@@ -61,7 +61,7 @@ static void *alloc(uint32_t size, uint8_t align)
 		expand(length + size);
 		uint32_t new_length = kheap->end_addr - kheap->start_addr;
 		iter = 0;
-		uint32_t idx = ARRAY_IDX_SIZE + 1;
+		uint32_t idx = -1;
 		uint32_t val = 0x0; // block
 		while (iter < kheap->arr.size)
 		{
@@ -73,7 +73,7 @@ static void *alloc(uint32_t size, uint8_t align)
 			}
 			iter++;
 		}
-		if (idx == ARRAY_IDX_SIZE + 1)
+		if (idx == -1)
 		{
 			header_t *header = (header_t *)end_addr;
 			header->is_hole = 1;
@@ -99,7 +99,7 @@ static void *alloc(uint32_t size, uint8_t align)
 	uint32_t hole_loc = (uint32_t)target_hole;
 	uint32_t hole_size = target_hole->size;
 	
-	// 요청된 크기만큼 나눠준 후에 hole의 크기가 header와 footer의 크기 합 보다 작은 경우 
+	// 요청된 크기만큼 나눠준 후, hole의 크기가 header와 footer의 크기 합 보다 작은 경우
 	if (hole_size - new_size < sizeof(header_t) + sizeof(footer_t))
 	{
 		size += hole_size - new_size;
@@ -108,20 +108,18 @@ static void *alloc(uint32_t size, uint8_t align)
 	if (align && (hole_loc & 0xFFFFF000))
 	{
 		uint32_t new_loc = hole_loc - (hole_loc & 0xFFF) + 0x1000 - sizeof(header_t);
-		header_t *header = (header_t *)hole_loc;
-		header->size = 0x1000 - (hole_loc & 0xFFF) - sizeof(header_t);
-		header->magic = HEAP_MAGIC;
-		header->is_hole = 1;
+		target_hole->size = 0x1000 - (hole_loc & 0xFFF) - sizeof(header_t);
 		footer_t *footer = (footer_t *)(new_loc - sizeof(footer_t));
-		footer->header = header;
+		footer->header = target_hole;
 		footer->magic = HEAP_MAGIC;
 		hole_loc = new_loc;
-		hole_size -= header->size;
+		hole_size -= target_hole->size;
 	}
 	else
 	{
 		delete_ordered_array(iter, &kheap->arr);
 	}
+	
 	header_t *block_header = (header_t *)hole_loc;
 	block_header->is_hole = 1;
 	block_header->size = new_size;
@@ -130,6 +128,25 @@ static void *alloc(uint32_t size, uint8_t align)
 	block_footer->header = block_header;
 	block_footer->magic = HEAP_MAGIC;
 
+	// hole의 크기가 block의 크기보다 작을 때, hole 만들기
+	if (hole_size - block_header->size > 0)
+	{
+		header_t *header = (header_t *)(hole_loc + block_header->size);
+		header->is_hole = 1;
+		header->magic = HEAP_MAGIC;
+		header->size = hole_size - block_header->size;
+		footer_t *footer = (footer_t *)(hole_loc + header->size - sizeof(footer_t));
+		if ((uint32_t)footer + sizeof(footer_t) >= kheap->end_addr)
+		{
+			uint32_t length = kheap->end_addr - kheap->start_addr;
+			uint32_t size = (uint32_t)footer - kheap->end_addr + sizeof(footer_t);
+			expand(length+size);
+		}
+		footer->header= header;
+		footer->magic = HEAP_MAGIC;
+		insert_ordered_array((type_t)header, &kheap->arr);
+	}
+	return (void *)((uint32_t)block_header + sizeof(header_t));
 }
 
 static void *kmalloc_init(uint32_t size, uint8_t align, uint32_t *phys)
